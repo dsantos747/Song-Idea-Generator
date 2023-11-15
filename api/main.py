2263ca8cb10a22f27e85f8cc19a92bcaa5945ab3
Daniel Santos <51920488+dsantos747@users.noreply.github.com>
2023-11-15 12:02:28 +0000

    Redis dependency removal (#3)
    * Initial commit to dev branch; removed Redis dependency
    
    * Fully removed redis dependency; corrected genre formatting (fixes #2)
    
    * Added console logging for vercel debugging
    
    * Switched to Flask in-memory session storage for Vercel
    
    * Added vercel secret key back in
    
    * Removed flask_session dependency
    
    * Added favicon

--- api/main.py
+++ api/main.py
@@ -1,17 +1,15 @@
-# Tasks
-# - Incorporate "popularity" or other filters into random song selection
-# - Run query based on genre, key and bpm - to find how many songs exist with that exact combination
-
 # Imports
 from dotenv import load_dotenv
 from flask import Flask, request, render_template, jsonify, session
-from flask_session import Session
+
+# from flask_session import Session
+from flask_cors import CORS
 import os
-import redis
 import base64
 from requests import post, get
 import json
 import random
+
 
 def get_token():
     auth_string = client_id + ":" + client_secret
@@ -21,7 +19,7 @@ def get_token():
     url = "https://accounts.spotify.com/api/token"
     headers = {
         "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/x-www-form-urlencoded",
     }
     data = {"grant_type": "client_credentials"}
     result = post(url, headers=headers, data=data)
@@ -29,8 +27,10 @@ def get_token():
     token = json_result["access_token"]
     return token
 
+
 def get_auth_header(token):
-    return{"Authorization": "Bearer " + token}
+    return {"Authorization": "Bearer " + token}
+
 
 def get_all_genres(token):
     url = f"https://api.spotify.com/v1/recommendations/available-genre-seeds/"
@@ -39,31 +39,47 @@ def get_all_genres(token):
     json_result = json.loads(result.content)["genres"]
     return json_result
 
-def convert_key(key,key_mode): # Convert Key in Pitch Class Notation into English
-    key_dict = {-1:"Unknown",0:"C",1:"C#",2:"D",3:"D#",4:"E",5:"F",6:"F#",7:"G",8:"G#",9:"A",10:"A#",11:"B"}
-    key_mode_dict = {0:"Minor", 1:"Major"}
+
+def convert_key(key, key_mode):  # Convert Key in Pitch Class Notation into English
+    key_dict = {
+        -1: "Unknown",
+        0: "C",
+        1: "C#",
+        2: "D",
+        3: "D#",
+        4: "E",
+        5: "F",
+        6: "F#",
+        7: "G",
+        8: "G#",
+        9: "A",
+        10: "A#",
+        11: "B",
+    }
+    key_mode_dict = {0: "Minor", 1: "Major"}
     return f"{key_dict[key]} {key_mode_dict[key_mode]}"
 
-def random_song(token,n,genres):
-    url_ids = "https://api.spotify.com/v1/recommendations"    
+
+def random_song(token, n, genres):
+    url_ids = "https://api.spotify.com/v1/recommendations"
     headers = get_auth_header(token)
 
-    dict_keys = ['ID', 'name', 'artist', 'genre', 'key', 'bpm', 'time_signature']
+    dict_keys = ["ID", "name", "artist", "genre", "key", "bpm", "time_signature"]
     song_dict_arr = [{key: None for key in dict_keys} for _ in range(n)]
     id_arr = []
 
     # Generate n random song ID's
     for item in song_dict_arr:
-        genre = random.choice(genres) 
-        choice = random.randint(0,19)   
+        genre = random.choice(genres)
+        choice = random.randint(0, 19)
         query = f"?seed_genres={genre}"
         query_url = url_ids + query
         result = get(query_url, headers=headers)
-        item['ID'] = json.loads(result.content)["tracks"][choice]['id']
-        id_arr.append(item['ID'])
-        item['genre'] = genre
+        item["ID"] = json.loads(result.content)["tracks"][choice]["id"]
+        id_arr.append(item["ID"])
+        item["genre"] = genre
 
-    id_list = '%2C'.join(id_arr)
+    id_list = "%2C".join(id_arr)
 
     # Use 1 API call to get track information for 3 songs, add to array>dictionary
     url_tracks = f"https://api.spotify.com/v1/tracks?ids={id_list}"
@@ -72,18 +88,28 @@ def random_song(token,n,genres):
     # Use 1 API call to get track audio features for 3 songs, add to array>dictionary
     url_audio_features = f"https://api.spotify.com/v1/audio-features?ids={id_list}"
     result_audio_features = get(url_audio_features, headers=headers)
-    
+
     # Add all info to the dict_arr
-    i=0
+    i = 0
     for item in song_dict_arr:
-        item['name'] = json.loads(result_tracks.content)["tracks"][i]["name"]
-        item['artist'] = json.loads(result_tracks.content)["tracks"][i]["artists"][0]['name']
-        item['key'] = convert_key(json.loads(result_audio_features.content)["audio_features"][i]["key"],json.loads(result_audio_features.content)["audio_features"][i]["mode"])
-        item['bpm'] = json.loads(result_audio_features.content)["audio_features"][i]["tempo"]
-        item['time_signature'] = f'{json.loads(result_audio_features.content)["audio_features"][i]["time_signature"]}/4'
-        i+=1
-    
+        item["name"] = json.loads(result_tracks.content)["tracks"][i]["name"]
+        item["artist"] = json.loads(result_tracks.content)["tracks"][i]["artists"][0][
+            "name"
+        ]
+        item["key"] = convert_key(
+            json.loads(result_audio_features.content)["audio_features"][i]["key"],
+            json.loads(result_audio_features.content)["audio_features"][i]["mode"],
+        )
+        item["bpm"] = json.loads(result_audio_features.content)["audio_features"][i][
+            "tempo"
+        ]
+        item[
+            "time_signature"
+        ] = f'{json.loads(result_audio_features.content)["audio_features"][i]["time_signature"]}/4'
+        i += 1
+
     return song_dict_arr
+
 
 # Pre-setup variables
 load_dotenv()
@@ -97,66 +123,68 @@ embed_url = "https://open.spotify.com/embed/track/"
 
 # Create the Flask app
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+CORS(app)
 
-# Session setup - for user genre filter choice
-if os.environ.get('FLASK_ENV') == 'production': # production deployment on vercel
-    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-    app.config['SESSION_TYPE'] = 'redis'
-    app.config['SESSION_PERMANENT'] = False
-    app.config['SESSION_USE_SIGNER'] = True
-    app.config['SESSION_KEY_PREFIX'] = 'song_gen_session'
-    app.config['SESSION_REDIS'] = redis.StrictRedis(host='redis-13815.c304.europe-west1-2.gce.cloud.redislabs.com', port=13815, db=0, password=os.getenv("DB_PASS"))
-else:
-    app.config['SESSION_TYPE'] = 'filesystem' # local development server, without redis 
-
-Session(app)
 
 # Define a route that handles a GET request
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html', genres_list=genres_list)
+    return render_template("index.html", genres_list=genres_list)
 
 
-@app.route('/post_genres', methods=['GET', 'POST'])
+@app.route("/post_genres", methods=["GET", "POST"])
 def submit_genres():
     if request.is_json:
-        # THE BELOW FOUR LINES ARE MY ORIGINAL CODE
         data = request.get_json()
-        chosen_genres = data['chosenGenresList']
-        session['selected_genres'] = [genre for genre in genres_list if genre in chosen_genres]
-        return jsonify({'message': 'Genres updated'})
+        chosen_genres = data["chosenGenresList"]
+        session["selected_genres"] = [
+            genre for genre in genres_list if genre in chosen_genres
+        ]
+        return jsonify(
+            {"message": "Genres updated", "genres": session["selected_genres"]}
+        )
 
     else:
-        return jsonify({'error': 'Unsupported Media Type'}), 415
+        return jsonify({"error": "Unsupported Media Type"}), 415
 
 
-@app.route('/get_songs', methods=['GET'])
+@app.route("/get_songs", methods=["GET"])
 def get_songs():
-    if request.method == 'GET':
-        print("request received")
+    if request.method == "GET":
+        selected_genres = session.get("selected_genres", genres_list)
+        song_arr = random_song(token, 3, selected_genres)
 
-        selected_genres=session.get('selected_genres',genres_list)
-        song_arr = random_song(token,3,selected_genres)
+        song_1_embed = embed_url + song_arr[0]["ID"]
+        song_1_name = song_arr[0]["name"]
+        song_1_artist = song_arr[0]["artist"]
+        song_1_genre = song_arr[0]["genre"]
+        song_2_embed = embed_url + song_arr[1]["ID"]
+        song_2_name = song_arr[1]["name"]
+        song_2_artist = song_arr[1]["artist"]
+        song_2_key = song_arr[1]["key"]
+        song_3_embed = embed_url + song_arr[2]["ID"]
+        song_3_name = song_arr[2]["name"]
+        song_3_artist = song_arr[2]["artist"]
+        song_3_bpm = song_arr[2]["bpm"]
+        song_3_time_signature = song_arr[2]["time_signature"]
 
-        song_1_embed = embed_url + song_arr[0]['ID']
-        song_1_name = song_arr[0]['name']
-        song_1_artist = song_arr[0]['artist']
-        song_1_genre = song_arr[0]['genre']
-        song_2_embed = embed_url + song_arr[1]['ID']
-        song_2_name = song_arr[1]['name']
-        song_2_artist = song_arr[1]['artist']
-        song_2_key = song_arr[1]['key']
-        song_3_embed = embed_url + song_arr[2]['ID']
-        song_3_name = song_arr[2]['name']
-        song_3_artist = song_arr[2]['artist']
-        song_3_bpm = song_arr[2]['bpm']
-        song_3_time_signature = song_arr[2]['time_signature']
-        
-        return jsonify(song_1_embed=song_1_embed,song_1_name=song_1_name,song_1_artist=song_1_artist,
-                       song_1_genre=song_1_genre,song_2_embed=song_2_embed,song_2_name=song_2_name,
-                       song_2_artist=song_2_artist,song_2_key=song_2_key,song_3_embed=song_3_embed,
-                       song_3_name=song_3_name,song_3_artist=song_3_artist,song_3_bpm=song_3_bpm,
-                       song_3_time_signature=song_3_time_signature)
+        return jsonify(
+            song_1_embed=song_1_embed,
+            song_1_name=song_1_name,
+            song_1_artist=song_1_artist,
+            song_1_genre=song_1_genre,
+            song_2_embed=song_2_embed,
+            song_2_name=song_2_name,
+            song_2_artist=song_2_artist,
+            song_2_key=song_2_key,
+            song_3_embed=song_3_embed,
+            song_3_name=song_3_name,
+            song_3_artist=song_3_artist,
+            song_3_bpm=song_3_bpm,
+            song_3_time_signature=song_3_time_signature,
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
--- api/static/main.js
+++ api/static/main.js
@@ -1,51 +1,56 @@
 // Define Buttons
-const generateButton = document.getElementById("generate");
-const genreButtons = document.querySelectorAll(".genre-button");
-const selectAllButton = document.getElementById("select-all");
-const showGenresButton = document.getElementById("show-genres");
+const generateButton = document.getElementById('generate');
+const genreButtons = document.querySelectorAll('.genre-button');
+const selectAllButton = document.getElementById('select-all');
+const showGenresButton = document.getElementById('show-genres');
 
 // Update all songs upon clicking "generate" button
-generateButton.addEventListener("click", function () {
+generateButton.addEventListener('click', function () {
   const chosenGenresList = getChosenGenres();
   const requestData = { chosenGenresList };
 
-  generateButton.textContent = "...thinking...";
+  generateButton.textContent = '...thinking...';
 
-  fetch("/post_genres", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+  console.log(requestData);
+
+  fetch('/post_genres', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(requestData),
   })
+    .then(console.log('posted genres'))
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      fetch("/get_songs", { method: "GET" })
+      fetch('/get_songs', { method: 'GET' })
         .then((response) => response.json())
         .then((data) => {
-          document.getElementById("song-1-embed").src = data.song_1_embed;
-          document.getElementById("song-1-genre").textContent = data.song_1_genre;
-          document.getElementById("song-2-embed").src = data.song_2_embed;
-          document.getElementById("song-2-key").textContent = data.song_2_key;
-          document.getElementById("song-3-embed").src = data.song_3_embed;
-          document.getElementById("song-3-bpm").textContent = data.song_3_bpm;
-          document.getElementById("song-3-time").textContent = data.song_3_time_signature;
-          generateButton.textContent = "Generate Songs";
+          document.getElementById('song-1-embed').src = data.song_1_embed;
+          document.getElementById('song-1-genre').textContent = data.song_1_genre;
+          document.getElementById('song-2-embed').src = data.song_2_embed;
+          document.getElementById('song-2-key').textContent = data.song_2_key;
+          document.getElementById('song-3-embed').src = data.song_3_embed;
+          document.getElementById('song-3-bpm').textContent = data.song_3_bpm;
+          document.getElementById('song-3-time').textContent = data.song_3_time_signature;
+          generateButton.textContent = 'Generate Songs';
         })
-        .catch((error) => console.error("Error:", error));
+        .catch((error) => console.error('Error:', error));
     });
 });
 
 // Update genre pool buttons when clicked
 genreButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    button.classList.toggle("clicked");
+  button.addEventListener('click', () => {
+    button.classList.toggle('clicked');
 
-    if (Array.from(genreButtons).some((button) => button.classList.contains("clicked"))) {
-      generateButton.textContent = "Generate Songs";
-      generateButton.removeAttribute("disabled");
+    if (Array.from(genreButtons).some((button) => button.classList.contains('clicked'))) {
+      generateButton.textContent = 'Generate Songs';
+      generateButton.style.fontWeight = 'bold';
+      generateButton.removeAttribute('disabled');
     } else {
-      generateButton.textContent = "Select at least 1 genre";
-      generateButton.setAttribute("disabled", "true");
+      generateButton.textContent = 'Select at least 1 genre';
+      generateButton.style.fontWeight = 'normal';
+      generateButton.setAttribute('disabled', 'true');
     }
   });
 });
@@ -55,7 +60,7 @@ function getChosenGenres() {
   const chosenGenres = [];
 
   genreButtons.forEach((genreButtons) => {
-    if (genreButtons.classList.contains("clicked")) {
+    if (genreButtons.classList.contains('clicked')) {
       chosenGenres.push(genreButtons.id);
     }
   });
@@ -64,28 +69,28 @@ function getChosenGenres() {
 }
 
 // Select all genres
-selectAllButton.addEventListener("click", () => {
-  const anyChecked = Array.from(genreButtons).some((button) => button.classList.contains("clicked"));
+selectAllButton.addEventListener('click', () => {
+  const anyChecked = Array.from(genreButtons).some((button) => button.classList.contains('clicked'));
 
   genreButtons.forEach((button) => {
     if (anyChecked) {
-      button.classList.remove("clicked");
-      generateButton.textContent = "Select at least 1 genre";
-      generateButton.setAttribute("disabled", "true");
+      button.classList.remove('clicked');
+      generateButton.textContent = 'Select at least 1 genre';
+      generateButton.setAttribute('disabled', 'true');
     } else {
-      button.classList.add("clicked");
-      generateButton.textContent = "Generate Songs";
-      generateButton.removeAttribute("disabled");
+      button.classList.add('clicked');
+      generateButton.textContent = 'Generate Songs';
+      generateButton.removeAttribute('disabled');
     }
   });
 });
 
 // Show/Hide Genre Pool
-showGenresButton.addEventListener("click", () => {
-  document.getElementById("genre-pool").classList.toggle("hidden");
-  if (document.getElementById("genre-pool").classList.contains("hidden")) {
-    showGenresButton.textContent = "Show genres";
+showGenresButton.addEventListener('click', () => {
+  document.getElementById('genre-pool').classList.toggle('hidden');
+  if (document.getElementById('genre-pool').classList.contains('hidden')) {
+    showGenresButton.textContent = 'Show genres';
   } else {
-    showGenresButton.textContent = "Hide genres";
+    showGenresButton.textContent = 'Hide genres';
   }
 });
